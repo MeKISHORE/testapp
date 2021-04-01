30315543b65481ddfe61d1625aded585165734e4
MeKISHORE <love.hembram@gmail.com>
2021-04-01 12:51:50 +0530

    first commit

---
+++ snake.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+bool gameover;
+using namespace std;
+
+const int width =20;
+const int height = 20;
+int x,y,fruitX,fruitY,score;
+enum eDirection{STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
+void setup(){
+
+    gameover = false;
+    dir = STOP;
+    x= width/2;
+    y= height/2;
+    fruitX = rand()%width;
+    fruitY = rand()%height;
+    score=0;
+}
+
+void draw(){
+
+    system("cls");
+    for(int i=0;i<width;i++);
+        cout<<"#";
+    cout<<endl;
+
+    for(int i=0;i<height;i++){
+        for(int j=0;j<width;j++){
+            if(j==0)
+                cout<<"#";
+            else
+                cout<<" ";
+            if(j==width-1)
+                cout<<"#";
+            
+        }
+    }
+
+
+    for(int i=0;i<width;i++);
+        cout<<"#";
+}
+
+void input(){
+
+
+
+}
+
+void logic(){
+
+
+
+
+}
+
+int main(){
+     setup();
+    while(!gameover){
+       draw();
+       input();
+       logic();
+    }
+
+
+
+
+}
